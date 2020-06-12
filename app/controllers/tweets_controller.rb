e28d6d6ab868d2ac56875093dc44ff70deacde5e
usumarin <ubuntu2682ch@gmail.com>
2020-06-13 08:36:47 +0900

    add : tweet投稿機能作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,23 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all.order(id: :DESC)
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:success] = "成功"
+    else
+      flash[:danger] = "失敗"
+      render "root_path"
+    end
+  end
+
+  private
+
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
   get '/tweets', to: 'tweets#index'
+  get '/new', to: 'tweets#new'
+  post '/tweets', to: 'tweets#create'
 end
