a621bc1c425c2062429cb6e2c33c69a5d7af444e
usumarin <ubuntu2682ch@gmail.com>
2020-06-25 11:05:00 +0900

    add : Relationshipモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_113018) do
+ActiveRecord::Schema.define(version: 2020_06_25_020251) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
