016c1489de03187481a2d7bced6c64d80052f97a
usumarin <ubuntu2682ch@gmail.com>
2020-06-25 11:58:29 +0900

    add : ユーザーフォロー機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/tweets', to: 'tweets#index'
   get '/new', to: 'tweets#new'
   post '/tweets', to: 'tweets#create'
+  resources :relationships, only: [:create, :destroy]
 end
