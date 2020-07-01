6e5e5c527b7f911e6b3344b74ba8ca970fd11d3c
usumarin <ubuntu2682ch@gmail.com>
2020-07-01 12:16:51 +0900

    update : ツイート一覧にユーザープロフィールページを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,6 @@ before_action :authenticate_user!, expect: :index
 
   def index
     @tweets = Tweet.all.order(id: :DESC)
-    @user = User.find_by(id: @tweets.user_id)
   end
 
   def new
@@ -13,7 +12,7 @@ before_action :authenticate_user!, expect: :index
   def create
     @tweet = Tweet.new(content: tweet_params[:content], user_id: current_user.id)
     if @tweet.save
-      flash[:success] = "成功"
+      flash[:success] = "戝功"
       redirect_to tweets_path
     else
       render "new"
