9d05c149eb98457eb302b6c9622e7ebc673e7906
usumarin <ubuntu2682ch@gmail.com>
2020-07-23 17:30:43 +0900

    hotfix : スペルミスでユーザー登録が出来ないバグを修正
    deviseのストロングパラメータの設定にスペルミスがありユーザー登録出来ないバグを修正
    ストロングパラメータを適切に設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :name, :password, :password_confirmation, :profile, :avatar ]
-    devise_parameter_sanitizer.permit :sin_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: [ :email, :name, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :account_update, keys: [ :email, :name, :password, :password_confirmation, :current_password, :profile, :avatar ]
+    devise_parameter_sanitizer.permit :sign_in, keys: [ :email, :password ]
   end
 end
