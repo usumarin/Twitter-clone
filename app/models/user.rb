8a75e7b52d6ebf84fc41dfdb3c25ef36f6c765cf
usumarin <ubuntu2682ch@gmail.com>
2020-07-23 12:56:31 +0900

    fix : 複数のユーザーが同じメールアドレスを使用することを不可に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :profile, length: { maximum: 200 }
+  validates :email, presence: true, uniqueness: true
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships 
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
