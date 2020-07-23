e4b0a8b64d69114f3c6c705c317331e238f0702c
usumarin <ubuntu2682ch@gmail.com>
2020-07-23 13:38:23 +0900

    update : ユーザーのセッション有効期限を12時間に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable,:timeoutable
   validates :name, presence: true
   validates :profile, length: { maximum: 200 }
   validates :email, presence: true, uniqueness: true
