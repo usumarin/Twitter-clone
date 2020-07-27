09f178de2078d55e1e0784941a67bbfdc494606c
usumarin <ubuntu2682ch@gmail.com>
2020-07-27 21:06:15 +0900

    update : 現在のパスワード無しに情報を更新可能に変更
    現在のパスワード無しに情報を更新可能に変更
    ユーザー編集ページのフォームの現在のパスワードを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,20 @@ class User < ApplicationRecord
     return self.avatar.variant(resize: '300x300').processed
   end
 
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   private
 
     def avatar_type
