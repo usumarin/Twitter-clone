4f2695be0361a0bcca5cf0c65e54d52f3926d834
usumarin <ubuntu2682ch@gmail.com>
2020-07-27 21:15:34 +0900

    update : ユーザーパスワードの最大文字数を16文字までに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:timeoutable
+         :recoverable, :rememberable, :validatable,:timeoutable, password_length: 6..16
   validates :name, length: { in: 1..10 }
   validates :profile, length: { maximum: 200 }
   validates :email, presence: true, uniqueness: true
